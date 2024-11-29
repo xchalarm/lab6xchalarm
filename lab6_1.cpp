4ab34b41da084938789f9fe41e7b69a7f26ff05f
xchalarm <tharana4671@gmail.com>
2024-11-29 15:50:04 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int countodd=0, counteven=0, num[30];
+    int i=0;
+    
+    while(1){
+        cout << "Enter an integer: ";
+        cin >> num[i] ; 
+        
+        if(num[i] == 0){
+            break;
+        }
+        if(num[i] % 2 == 0){
+            counteven++;
+        }else{
+            countodd++ ;
+        }
+        i++;
+    }
+    cout << "#Even numbers = "<< counteven << endl;
+    cout << "#Odd numbers = " << countodd ;
     return 0;
 }
+
